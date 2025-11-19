ff112d4f24bf34e9639625dffae28267c86a942a
yufan <wangyufan22@mails.ucas.ac.cn>
2025-11-19 09:19:49 +0800

    Update downloadSRR.bash

--- downloadSRR.bash
+++ downloadSRR.bash
@@ -5,5 +5,5 @@ cd /data01/wangyf/project2
 conda activate sratoolkit
 
 prefetch SRR26304028 && fasterq-dump --threads 10 --split-3 ./1.SRRdata/SRR8380186/SRR8380186.sra -O ./SRR8380186
-prefetch --option-file download.list && fasterq-dump --threads 10 --split-3 ./1.SRRdata/SRR8380186/SRR8380186.sra -O ./SRR8380186
+prefetch --option-file download.list --max-size 50G  # && fasterq-dump --threads 10 --split-3 ./1.SRRdata/SRR8380186/SRR8380186.sra -O ./SRR8380186
 gzip SRR8380186_1.fastq && gzip SRR8380186_2.fastq
